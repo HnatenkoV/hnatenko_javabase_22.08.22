9723b6d5b281f6d8c5217b38e4d6ad1f086905a9
Hnatenkov <hnatenko.vadim3@gmail.com>
2022-08-22 23:30:05 +0300

    + lwsson3(main)

---
+++ src/main/java/com/hillel/hnatenko/lesson/lesson3/Main.java
@@ -0,0 +1,20 @@
+package com.hillel.hnatenko.lesson.lesson3;
+
+public class Main {
+    public static void main (String[] args) {
+
+        int a = 5;
+        int b = 3;
+        int h = 8;
+
+        int volume = a*b*h;
+
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println();
+
+        int length = 4*(a+b+h);
+
+        System.out.println("Cума довжин паралелепіпеда = " + length);
+
+    }
+}
