b2abe6b4f11b4cc8ab21d5517881649f8c07beb7
Hnatenkov <hnatenko.vadim3@gmail.com>
2022-08-30 22:16:33 +0300

    + lwsson5(main)

---
+++ src/main/java/com/hillel/hnatenko/lesson/lesson2/MyCoord.java
@@ -0,0 +1,11 @@
+package com.hillel.hnatenko.lesson.lesson2;
+
+public class MyCoord {
+    public static void main (String[] args) {
+
+        double doubleVariable = 48.40280630573652;
+        double doubleVariable2 = 35.131653012437226;
+
+        System.out.println("My coordinates " + doubleVariable + "\u002C"+ doubleVariable2);
+    }
+}
---
+++ src/main/java/com/hillel/hnatenko/lesson/lesson5/Main.java
@@ -0,0 +1,135 @@
+package com.hillel.hnatenko.lesson.lesson5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter first team name");
+        String firstTeam = scanner.nextLine();
+        System.out.println("You entered: " + firstTeam);
+
+        int firstTeamPlayer1 = -1;
+        System.out.println("Please enter frags for firstTeamPlayer1");
+        if (scanner.hasNextInt()) {
+            firstTeamPlayer1 = scanner.nextInt();
+            System.out.println("You entered number: " + firstTeamPlayer1);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int firstTeamPlayer2 = -1;
+        System.out.println("Please enter frags for firstTeamPlayer2");
+        if (scanner.hasNextInt()) {
+            firstTeamPlayer2 = scanner.nextInt();
+            System.out.println("You entered number: " + firstTeamPlayer2);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int firstTeamPlayer3 = -1;
+        System.out.println("Please enter frags for firstTeamPlayer3");
+        if (scanner.hasNextInt()) {
+            firstTeamPlayer3 = scanner.nextInt();
+            System.out.println("You entered number: " + firstTeamPlayer3);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int firstTeamPlayer4 = -1;
+        System.out.println("Please enter frags for firstTeamPlayer4");
+        if (scanner.hasNextInt()) {
+            firstTeamPlayer4 = scanner.nextInt();
+            System.out.println("You entered number: " + firstTeamPlayer4);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int firstTeamPlayer5 = -1;
+        System.out.println("Please enter frags for firstTeamPlayer5");
+        if (scanner.hasNextInt()) {
+            firstTeamPlayer5 = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("You entered number: " + firstTeamPlayer5);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        System.out.println("Please enter second team name");
+        String secondTeam = scanner.nextLine();
+        System.out.println("You entered: " + secondTeam);
+
+        int secondTeamPlayer1 = -1;
+        System.out.println("Please enter frags for secondTeamPlayer1");
+        if (scanner.hasNextInt()) {
+            secondTeamPlayer1 = scanner.nextInt();
+            System.out.println("You entered number: " + secondTeamPlayer1);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int secondTeamPlayer2 = -1;
+        System.out.println("Please enter frags for secondTeamPlayer2");
+        if (scanner.hasNextInt()) {
+            secondTeamPlayer2 = scanner.nextInt();
+            System.out.println("You entered number: " + secondTeamPlayer2);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int secondTeamPlayer3 = -1;
+        System.out.println("Please enter frags for secondTeamPlayer3");
+        if (scanner.hasNextInt()) {
+            secondTeamPlayer3 = scanner.nextInt();
+            System.out.println("You entered number: " + secondTeamPlayer3);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int secondTeamPlayer4 = -1;
+        System.out.println("Please enter frags for secondTeamPlayer4");
+        if (scanner.hasNextInt()) {
+            secondTeamPlayer4 = scanner.nextInt();
+            System.out.println("You entered number: " + secondTeamPlayer4);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int secondTeamPlayer5 = -1;
+        System.out.println("Please enter frags for secondTeamPlayer5");
+        if (scanner.hasNextInt()) {
+            secondTeamPlayer5 = scanner.nextInt();
+            System.out.println("You entered number: " + secondTeamPlayer5);
+        } else {
+            System.out.println("WRONG DATA, RESTART!");
+            System.exit(0);
+        }
+
+        int firstTeamResult =(firstTeamPlayer1 + firstTeamPlayer2 + firstTeamPlayer3 + firstTeamPlayer4 + firstTeamPlayer5);
+        double firstTeamScore = (double)  firstTeamResult/ 5;
+        int secondTeamResult = (secondTeamPlayer1 + secondTeamPlayer2 + secondTeamPlayer3 + secondTeamPlayer4 + secondTeamPlayer5);
+        double secondTeamScore = (double)  secondTeamResult/ 5;
+
+        if (firstTeamResult > secondTeamResult) {
+              System.out.println("Перемогла команда " + firstTeam + " набравши " + firstTeamScore + " очків");
+          } else if (firstTeamResult < secondTeamResult) {
+              System.out.println("Перемогла команда " + secondTeam + " набравши " + secondTeamScore + " очків");
+        } else if (firstTeamResult == secondTeamResult) {
+              System.out.println("Воу воу та це ж нічия " + firstTeam  + " і " + secondTeam  + " набрали однакову кількість очків");
+        }
+
+        scanner.close();
+
+    }
+}
